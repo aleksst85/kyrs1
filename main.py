bb4ee2e99593996aea7f7755a2d5fc72b5564b86
aleksst85 <65343095+aleksst85@users.noreply.github.com>
2023-02-20 17:59:44 +0700

    изменения

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ path = "quest.json"
 from util import load_json
 from quest_class import Question
 
-word, subword = load_json(path)
-print("Ваше слово", word)
-quiestion = Question(word, subword)
+print('Введите имя игрока:', end=' ')
+name = input()
+print(f"Привет {name} начнем игру!")
+print(f"{name} составь из слова {word} {len(subword)} слов!")
+print("Слова должны быть не короче 3 букв")
+print("Для выхода из игры угадайте все слова")
+print("или введите слово стоп")
+quiestion = load_json(path)
 
 while quiestion.in_correct(input()):
     print(end='')
--- util.py
+++ util.py
@@ -1,8 +1,9 @@
 import json
 from random import randint
+from quest_class import Question
 def load_json(path):
     with open(path, "r") as file:
         data_json = file.read()
     words = json.loads(data_json)
     list_word = (words[randint(0, len(words) - 1)])
-    return list_word['word'], list_word['subword']
+    return Question(list_word['word'], list_word['subword'])
