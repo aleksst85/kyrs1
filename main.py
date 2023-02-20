39f0f9b5cf9fafcda4cb0b317984509aba8952cf
aleksst85 <65343095+aleksst85@users.noreply.github.com>
2023-02-20 18:33:03 +0700

    изменения

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from quest_class import Question
 
 print('Введите имя игрока:', end=' ')
 name = input()
+quiestion = load_json(path)
 print(f"Привет {name} начнем игру!")
-print(f"{name} составь из слова {word} {len(subword)} слов!")
+print(f"{name} составь из слова >>>>{quiestion.word}<<<< {len(quiestion.subword)} слов!")
 print("Слова должны быть не короче 3 букв")
 print("Для выхода из игры угадайте все слова")
 print("или введите слово стоп")
-quiestion = load_json(path)
 
 while quiestion.in_correct(input()):
     print(end='')
-
+print(f"Вы угадали {len(quiestion.open_word)} из {len(quiestion.subword)} возможных слов")
 
 
 
--- quest_class.py
+++ quest_class.py
@@ -4,16 +4,21 @@ class Question():
         self.subword = subword
         self.open_word = list()
 
+    def __repr__(self):
+        return f'Класс для {self.word}'
+
     def in_correct(self, user_input):
         self.user_input= user_input
         if self.user_input in  self.subword and self.user_input not in self.open_word:
             print('Правильно')
             self.open_word.append(self.user_input)
             if len(self.subword) == len(self.open_word):
-                print('Поздравляем вы угадали все слова')
                 return False
             else:
                 return True
+        elif len(self.user_input)<3:
+            print('Слишком короткое слово!')
+            return True
         elif self.user_input in self.open_word:
             print('Это слово уже было')
             return True
