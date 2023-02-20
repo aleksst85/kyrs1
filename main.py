623f4e64f9c9c50fa116364ebe7be9bb2e57932e
aleksst85 <65343095+aleksst85@users.noreply.github.com>
2023-02-20 08:29:27 +0700

    изменения

--- main.py
+++ main.py
@@ -3,24 +3,12 @@ import json
 
 
 
-file = open("quest.json")
-words=json.load(file)
-file.close()
+with open("quest.json", "r") as file:
+    data_json = file.read()
+words = json.loads(data_json)
 for word in words:
-    for elem in word:
-        print(elem)
-
+    print(word)
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
